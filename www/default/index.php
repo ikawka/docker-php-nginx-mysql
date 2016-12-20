2f050fe19cb15bd15a0334bb13ac492a9028aa36
Amiel <pixelazion@gmail.com>
2016-12-20 14:26:54 +0800

    updated Dockerfile and Readme

--- www/default/index.php
+++ www/default/index.php
@@ -1,8 +1,1 @@
-<?php //phpinfo();
-$servers = explode(",", ini_get("session.save_path"));
-$c = count($servers);
-for ($i = 0; $i < $c; ++$i) {
-  $servers[$i] = explode(":", $servers[$i]);
-}
-
-print_r($servers);
+<?php phpinfo();
